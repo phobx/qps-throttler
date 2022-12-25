5b53686bb665e46540a7815da93d66f1b6f88f10
Yaroslav Hulaha <gulaga.yaroslav@gmail.com>
2022-12-25 12:08:14 +0100

    add initial code

---
+++ src/main/java/org/example/QpsThrottler.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class QpsThrottler {
+
+    public boolean pass() {
+        throw new RuntimeException("Not implemented.");
+    }
+}
---
+++ src/test/java/org/example/QpsThrottlerTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QpsThrottlerTest {
+
+    @org.junit.jupiter.api.Test
+    void pass() {
+        throw new RuntimeException("Not implemented.");
+    }
+}

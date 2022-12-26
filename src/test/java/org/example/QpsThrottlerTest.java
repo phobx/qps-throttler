d125f058f85846edc78993c62fe50813bc798f9c
Yaroslav Hulaha <gulaga.yaroslav@gmail.com>
2022-12-26 19:27:34 +0100

    add unit test with joined threads

--- src/test/java/org/example/QpsThrottlerTest.java
+++ src/test/java/org/example/QpsThrottlerTest.java
@@ -1,11 +1,37 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class QpsThrottlerTest {
 
-    @org.junit.jupiter.api.Test
-    void pass() {
-        throw new RuntimeException("Not implemented.");
+    private int numberOfThreads = 5;
+    private final QpsThrottler qpsThrottler = new QpsThrottler();
+    private final AtomicInteger passCount = new AtomicInteger();
+    private final AtomicInteger successfulPassCount = new AtomicInteger();
+
+    @Test
+    void testPass() {
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new QpsThrottlerTestingThread(qpsThrottler, passCount, successfulPassCount);
+            threads.add(thread);
+        }
+        Long startTs = System.currentTimeMillis();
+        threads.stream().forEach(Thread::start);
+        threads.stream().forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException ie) {
+                throw new RuntimeException("InterruptedException caught.");
+            }
+        });
+        Long endTs = System.currentTimeMillis();
+        System.out.println("Total time in millis: " + (endTs - startTs));
+        System.out.println("Total invocations: " + passCount.get());
+        System.out.println("Total successful invocations: " + successfulPassCount.get());
     }
 }
